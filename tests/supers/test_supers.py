98b3db267da187548d7d700d9de0b91a3c5f6e3a
Quien Sabe <qs5779@mail.com>
2023-01-27 16:23:08 -0400

    chore: Corrected formatting

--- tests/supers/test_supers.py
+++ tests/supers/test_supers.py
@@ -1,12 +1,15 @@
 """Test module for wtforglib package."""
 import platform
+
 import pytest
+
 from wtforglib.supers import issuper, requires_super_user
 
 WINDOZE = "Windows"
 
 if platform.system() == WINDOZE:
     pytest.skip("skipping tests on windows platform", allow_module_level=True)
+
 
 def test_issuper():
     """Test issuper function."""
