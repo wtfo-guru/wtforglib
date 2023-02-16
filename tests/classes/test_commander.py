a185fda1f485ea4034ca14722b80e4a69e72b249
Quien Sabe <qs5779@mail.com>
2023-02-16 14:21:59 -0400

    tests: Support windows tests

--- tests/classes/test_commander.py
+++ tests/classes/test_commander.py
@@ -1,6 +1,7 @@
 """Test module for wtforglib package."""
+import platform
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from wtforglib.commander import Commander, FakedProcessResult
 
@@ -8,8 +9,13 @@ from wtforglib.commander import Commander, FakedProcessResult
 def test_date_command():
     """Test date command."""
     cmdr = Commander()
-    res = cmdr.run_command(("date", "+%s"), always=True)  # noqa: WPS323
-    tstamp = datetime.now().timestamp()
+    if platform.system() == 'Windows':
+        args = ("powershell", "-noprofile", "[int](Get-Date -UFormat %s -Millisecond 0)")
+    else:
+        args = ("date", "+%s")
+    res = cmdr.run_command(args, always=True)  # noqa: WPS323
+    # tstamp = datetime.now().timestamp()
+    tstamp = int((datetime.now() - datetime(1970, 1, 1)) / timedelta(seconds=1))
     assert res.returncode == 0
     assert tstamp - int(res.stdout.strip()) <= 1
 
