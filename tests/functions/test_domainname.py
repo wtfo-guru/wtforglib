82b411f0e4ceeb5971f78f24906a6e22f4f3b83e
Quien Sabe <qs5779@mail.com>
2025-05-08 12:29:12 -0400

    resettable singleton class decorator (#238)
    * feat: resettable singleton class decorator
    
    * test: added version test for coverage
    
    * test: added tomli requirement
    
    * test: removed safety scan from ci
    
    * test: fixed resettable test
    
    ---------
    
    Co-authored-by: Quien Sabe <qs5579@mail.com>

--- tests/functions/test_domainname.py
+++ tests/functions/test_domainname.py
@@ -1,9 +1,26 @@
-from wtforglib.functions import domainname
+"""Tests.functions.test_domainname module for wtforglib package."""
 
 # mypy: disable_error_code = var-annotated
+
+from pathlib import Path
+
+import tomli
+
+from wtforglib.constants import VERSION
+from wtforglib.functions import domainname
 
 
 def test_domainname():
     """Test domainname function."""
     assert domainname(True) == "example.com"
     assert domainname() == "example.com"
+
+
+def test_version():
+    """Test version function."""
+    version: str
+    with open(
+        Path(__file__).parent.parent.parent.resolve() / "pyproject.toml", mode="rb"
+    ) as pyproject:
+        version = tomli.load(pyproject)["project"]["version"]
+    assert VERSION == version
--- wtforglib/constants.py
+++ wtforglib/constants.py
@@ -1,3 +1,3 @@
 """Top-level module for wtforglib Library."""
 
-VERSION = "1.2.0"
+VERSION = "1.2.1"
